6ff970f74c055d22cfaae9d04ca82585cad4a771
Emanuele Bardelli <bardellie@gmail.com>
2023-12-02 09:48:58 -0800

    Solve Day 1 Part 2
    Was there a part 2???

--- 2023/day1/day1.sql
+++ 2023/day1/day1.sql
@@ -1,4 +1,4 @@
-select 
+SELECT 
 	SUM(
 		CAST(
 			CONCAT(
@@ -6,5 +6,54 @@ select
 				right(regexp_replace(column0, '[^0-9]+$', ''), 1)
 			) 
 		AS INT)
-	) AS solution
-from 'day1/day1.csv';
+	) AS solution_part1
+FROM read_csv('day1/input', columns = {'column0': 'VARCHAR'});
+
+WITH 
+	data AS (
+		SELECT
+			replace(
+			replace(
+			replace(
+			replace(
+			replace(
+			replace(
+			replace(
+			replace(
+			replace(
+			replace(
+			replace(
+			replace(
+			replace(
+			replace(
+			replace(
+			column0, 'eightwo', '82'), 
+				     'eightree', '83'), 
+					 'oneight', '18'), 
+			         'threeight', '83'), 
+			 		 'fiveight', '85'),
+					 'twone', '21'),
+					 'one', '1'), 
+					 'two', '2'), 
+				     'three', '3'), 
+					 'four', '4'), 
+					 'five', '5'), 
+					 'six', '6'), 
+					 'seven', '7'), 
+					 'eight', '8'), 
+					 'nine', '9') 
+			AS c
+		FROM read_csv('day1/input', columns = {'column0': 'VARCHAR'})
+	)
+
+SELECT 
+	SUM(
+		CAST(
+			CONCAT(
+				left(regexp_replace(c, '^[^0-9]+', ''), 1), 
+				right(regexp_replace(c, '[^0-9]+$', ''), 1)
+			) 
+		AS INT)
+	) AS solution_part2
+FROM data
+;

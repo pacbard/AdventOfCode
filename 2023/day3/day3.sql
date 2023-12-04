1a7b15d8e478d4801f33a8d77bb0075f98816a55
Emanuele Bardelli <bardellie@gmail.com>
2023-12-03 20:04:46 -0800

    Solve day 3 part 2
    Holy moly Batman, this was bad. It seems that there is a weird thing going
    on with duckdb that messed up the ordering of the numbers. The first was
    my fault for not using generate_subscript. The second was some weird thing
    with CTEs. It seems that duckdb re-evaluates them every time you call them,
    so the joins in gear_ratios were using different tables even if I was calling
    the same one. That's weird.

--- 2023/day3/day3.sql
+++ 2023/day3/day3.sql
@@ -10,29 +10,24 @@ import AS (
 numbers AS (
 	SELECT
 		row,
-		unnest(schematics) as number
+		unnest(schematics) as number,
+		generate_subscripts(schematics, 1) AS loc
 	FROM
 		import
-),
-number_row AS (
-	SELECT
-		numbers.row AS row,
-		ROW_NUMBER() OVER (PARTITION BY row) AS loc,
-		regexp_replace(number, '[^0-9]', '.') AS number
-	FROM 
-		numbers
 ),
 number_filter AS (
 	SELECT
 		row,
 		loc,
-		lag(loc, 1, 1) OVER(PARTITION BY row) AS lag,
+		lag(loc, 1, 1) OVER(PARTITION BY row ORDER BY row, loc) AS lag,
 		number
 	FROM 
-		number_row
+		numbers
 
 	WHERE
 		number IN ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+
+	ORDER BY row, loc
 ),
 number_group AS (
 	SELECT
@@ -43,6 +38,8 @@ number_group AS (
 		
 	FROM 
 		number_filter
+
+	ORDER BY row, loc
 ),
 number_groups AS (
 	SELECT
@@ -53,43 +50,50 @@ number_groups AS (
 		
 	FROM 
 		number_group
+
+	ORDER BY row, loc
 ),
-number_location AS (
-	SELECT DISTINCT
+number_candidates AS (
+	SELECT
 		row,
-		CAST(min(loc) AS INT) AS loc_start,
-		CAST(max(loc) AS INT) AS loc_end,
-		CAST(string_agg(number, '') AS INT) AS number
+		min(loc) OVER (PARTITION BY row, ngroup) AS loc_start,
+		max(loc) OVER (PARTITION BY row, ngroup) AS loc_end,
+		CAST(string_agg(number, '') OVER (PARTITION BY row, ngroup ORDER BY row, ngroup, loc) AS INT) AS number
 
 	FROM
 		number_groups
 
-	GROUP BY
-		row, ngroup
+	ORDER BY row, loc
+),
+number_location AS (
+	SELECT DISTINCT
+		row,
+		loc_start,
+		loc_end,
+		MAX(number) OVER (PARTITION BY row, loc_start, loc_end) AS number
+
+	FROM
+		number_candidates
+
+	ORDER BY row, loc_start
 ),
 symbols AS (
 	SELECT
 		row,
-		unnest(schematics) as symbol
+		unnest(schematics) AS symbol,
+		generate_subscripts(schematics, 1) AS loc
 	FROM
 		import
 ),
-symbol AS (
-	SELECT
-		*,
-		ROW_NUMBER() OVER (PARTITION BY row) AS loc
-	FROM
-		symbols
-),
 symbol_location AS (
 	SELECT DISTINCT
-		symbol.row AS row,
+		row AS row,
 		loc,
-		symbol.symbol AS symbol
-	FROM 
 		symbol
+	FROM 
+		symbols
 	WHERE 
-		symbol.symbol NOT IN ('.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+		symbols.symbol NOT IN ('.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
 ),
 list AS (
 	SELECT
@@ -120,11 +124,182 @@ list AS (
 		AND
 		symbol_location.loc BETWEEN number_location.loc_start - 1 AND number_location.loc_end + 1
 )
---535078
-
--- SELECT * FROM list ORDER BY row, loc_start
-
 SELECT
 	SUM(number) AS solution_part1
-FROM list
-;
+FROM list;
+
+-- Part 2
+WITH
+import AS (
+	SELECT 
+		ROW_NUMBER() OVER () AS row,
+		regexp_extract_all(schematics, '(.)', 1) AS schematics,
+	FROM read_csv('day3/input', columns = {'schematics': 'VARCHAR'})
+),
+numbers AS (
+	SELECT
+		row,
+		unnest(schematics) as number,
+		generate_subscripts(schematics, 1) AS loc
+	FROM
+		import
+),
+number_filter AS (
+	SELECT
+		row,
+		loc,
+		lag(loc, 1, 1) OVER(PARTITION BY row ORDER BY row, loc) AS lag,
+		number
+	FROM 
+		numbers
+
+	WHERE
+		number IN ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+
+	ORDER BY row, loc
+),
+number_group AS (
+	SELECT
+		row,
+		loc,
+		CASE WHEN loc =  lag + 1 THEN 0 ELSE 1 END AS group_bump,
+		number
+		
+	FROM 
+		number_filter
+
+	ORDER BY row, loc
+),
+number_groups AS (
+	SELECT
+		row,
+		CAST(loc AS INT) AS loc,
+		SUM(group_bump) OVER (ORDER BY row, loc) AS ngroup,
+		number
+		
+	FROM 
+		number_group
+
+	ORDER BY row, loc
+),
+number_candidates AS (
+	SELECT
+		row,
+		min(loc) OVER (PARTITION BY row, ngroup) AS loc_start,
+		max(loc) OVER (PARTITION BY row, ngroup) AS loc_end,
+		CAST(string_agg(number, '') OVER (PARTITION BY row, ngroup ORDER BY row, ngroup, loc) AS INT) AS number
+
+	FROM
+		number_groups
+
+	ORDER BY row, loc
+),
+number_location AS (
+	SELECT DISTINCT
+		row,
+		loc_start,
+		loc_end,
+		MAX(number) OVER (PARTITION BY row, loc_start, loc_end) AS number
+
+	FROM
+		number_candidates
+
+	ORDER BY row, loc_start
+),
+symbols AS (
+	SELECT
+		row,
+		unnest(schematics) AS symbol,
+		generate_subscripts(schematics, 1) AS loc
+	FROM
+		import
+),
+symbol_location AS (
+	SELECT DISTINCT
+		row AS row,
+		loc,
+		symbol
+	FROM 
+		symbols
+	WHERE 
+		symbols.symbol NOT IN ('.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+),
+list AS (
+	SELECT
+		symbol,
+		symbol_location.row,
+		symbol_location.loc,
+		number_location.number
+	FROM number_location
+	JOIN symbol_location 
+		ON symbol_location.row = number_location.row
+		AND
+		symbol_location.loc BETWEEN number_location.loc_start - 1 AND number_location.loc_end + 1
+		AND
+		symbol_location.symbol = '*'
+
+	UNION
+
+	SELECT 
+		symbol,
+		symbol_location.row,
+		symbol_location.loc,
+		number_location.number
+	FROM number_location
+	JOIN symbol_location 
+		ON symbol_location.row = number_location.row + 1
+		AND
+		symbol_location.loc BETWEEN number_location.loc_start - 1 AND number_location.loc_end + 1
+		AND
+		symbol_location.symbol = '*'
+
+	UNION
+
+	SELECT 
+		symbol,
+		symbol_location.row,
+		symbol_location.loc,
+		number_location.number
+	FROM number_location
+	JOIN symbol_location 
+		ON symbol_location.row = number_location.row - 1
+		AND
+		symbol_location.loc BETWEEN number_location.loc_start - 1 AND number_location.loc_end + 1
+		AND
+		symbol_location.symbol = '*'
+),
+possible_gears AS (
+	SELECT 
+		symbol,
+		row,
+		loc,
+		number,
+		ROW_NUMBER() OVER (PARTITION BY symbol, row, loc ORDER BY symbol, row, loc) AS num
+	FROM list
+	ORDER BY symbol, row, loc
+),
+gear_ratios AS (
+SELECT 
+	possible_gears.symbol,
+	possible_gears.row,
+	possible_gears.loc,
+	possible_gears.number AS gear1,
+	possible_gears2.number AS gear2,
+	IFNULL(possible_gears.number, 0) * IFNULL(possible_gears2.number, 0) AS ratio
+
+	FROM possible_gears
+	JOIN possible_gears AS possible_gears2 
+	ON 
+		possible_gears.row = possible_gears2.row
+		AND 
+		possible_gears.loc = possible_gears2.loc
+		AND
+		possible_gears2.num = 2
+		AND
+		possible_gears.number <> possible_gears2.number
+)
+-- 89211325 too high
+-- 82660900 too high
+-- 75312571
+
+SELECT SUM(ratio) AS solution_part2 FROM gear_ratios
--- 2023/day3/test.sql
+++ 2023/day3/test.sql
@@ -4,106 +4,307 @@ WITH
 import AS (
 	SELECT 
 		ROW_NUMBER() OVER () AS row,
-		schematics,
-		CONCAT('.', regexp_replace(schematics, '([^0-9\.])', '.', 'g'), '.') as schematics_location
+		regexp_extract_all(schematics, '(.)', 1) AS schematics,
 	FROM read_csv('day3/test', columns = {'schematics': 'VARCHAR'})
 ),
 numbers AS (
 	SELECT
 		row,
-		regexp_extract_all(schematics, '([0-9]+)', 1) AS numbers
+		unnest(schematics) as number,
+		generate_subscripts(schematics, 1) AS loc
 	FROM
 		import
 ),
-number AS (
+number_filter AS (
 	SELECT
-		numbers.row,
-		unnest(numbers) as number
+		row,
+		loc,
+		lag(loc, 1, 1) OVER(PARTITION BY row ORDER BY row, loc) AS lag,
+		number
 	FROM 
 		numbers
+
+	WHERE
+		number IN ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+
+	ORDER BY row, loc
+),
+number_group AS (
+	SELECT
+		row,
+		loc,
+		CASE WHEN loc =  lag + 1 THEN 0 ELSE 1 END AS group_bump,
+		number
+		
+	FROM 
+		number_filter
+
+	ORDER BY row, loc
+),
+number_groups AS (
+	SELECT
+		row,
+		CAST(loc AS INT) AS loc,
+		SUM(group_bump) OVER (ORDER BY row, loc) AS ngroup,
+		number
+		
+	FROM 
+		number_group
+
+	ORDER BY row, loc
+),
+number_candidates AS (
+	SELECT
+		row,
+		min(loc) OVER (PARTITION BY row, ngroup) AS loc_start,
+		max(loc) OVER (PARTITION BY row, ngroup) AS loc_end,
+		CAST(string_agg(number, '') OVER (PARTITION BY row, ngroup ORDER BY row, ngroup, loc) AS INT) AS number
+
+	FROM
+		number_groups
+
+	ORDER BY row, loc
 ),
 number_location AS (
-	SELECT
-		number.row AS row,
-		CAST(number AS INT) AS number,
-		instr(schematics_location, CONCAT('.', number, '.')) - 1 AS loc_start,
-		instr(schematics_location, CONCAT('.', number, '.')) + strlen(number) + 1 AS loc_end
-	FROM 
-		number
-	JOIN
-		import ON import.row = number.row
+	SELECT DISTINCT
+		row,
+		loc_start,
+		loc_end,
+		MAX(number) OVER (PARTITION BY row, loc_start, loc_end) AS number
+
+	FROM
+		number_candidates
+
+	ORDER BY row, loc_start
 ),
 symbols AS (
 	SELECT
 		row,
-		regexp_extract_all(schematics, '(.)', 1) AS symbols
+		unnest(schematics) AS symbol,
+		generate_subscripts(schematics, 1) AS loc
 	FROM
 		import
 ),
-symbol_row AS (
-	SELECT
-		symbols.row,
-		unnest(symbols) as symbol
-	FROM 
-		symbols
-),
-symbol AS (
-	SELECT
-		*,
-		ROW_NUMBER() OVER () AS loc
-	FROM
-		symbol_row
-),
 symbol_location AS (
 	SELECT DISTINCT
-		symbol.row AS row,
-		(symbol.loc % 10) AS loc,
-		symbol.symbol AS symbol
-	FROM 
+		row AS row,
+		loc,
 		symbol
+	FROM 
+		symbols
 	WHERE 
-		symbol.symbol NOT IN ('.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+		symbols.symbol NOT IN ('.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
 ),
 list AS (
 	SELECT
-		number_location.row,
-		number_location.loc_start,
-		number_location.loc_end,
 		number_location.number
 	FROM number_location
 	JOIN symbol_location 
 		ON symbol_location.row = number_location.row
 		AND
-		symbol_location.loc BETWEEN number_location.loc_start AND number_location.loc_end
+		symbol_location.loc BETWEEN number_location.loc_start - 1 AND number_location.loc_end + 1
 
-	UNION
+	UNION ALL
 
 	SELECT 
-		number_location.row,
-		number_location.loc_start,
-		number_location.loc_end,
 		number_location.number
 	FROM number_location
 	JOIN symbol_location 
 		ON symbol_location.row = number_location.row + 1
 		AND
-		symbol_location.loc BETWEEN number_location.loc_start AND number_location.loc_end
+		symbol_location.loc BETWEEN number_location.loc_start - 1 AND number_location.loc_end + 1
 
-	UNION
+	UNION ALL
 
 	SELECT 
-		number_location.row,
-		number_location.loc_start,
-		number_location.loc_end,
 		number_location.number
 	FROM number_location
 	JOIN symbol_location 
 		ON symbol_location.row = number_location.row - 1
 		AND
-		symbol_location.loc BETWEEN number_location.loc_start AND number_location.loc_end
+		symbol_location.loc BETWEEN number_location.loc_start - 1 AND number_location.loc_end + 1
 )
 
 SELECT
-	SUM(number) AS solution_part1
-FROM list
+	4361 AS answer,
+	SUM(number) AS solution_part1,
+	4361 = SUM(number) AS right
+FROM list;
+
+-- Part 2
+WITH
+import AS (
+	SELECT 
+		ROW_NUMBER() OVER () AS row,
+		regexp_extract_all(schematics, '(.)', 1) AS schematics,
+	FROM read_csv('day3/test', columns = {'schematics': 'VARCHAR'})
+),
+numbers AS (
+	SELECT
+		row,
+		unnest(schematics) as number,
+		generate_subscripts(schematics, 1) AS loc
+	FROM
+		import
+),
+number_filter AS (
+	SELECT
+		row,
+		loc,
+		lag(loc, 1, 1) OVER(PARTITION BY row ORDER BY row, loc) AS lag,
+		number
+	FROM 
+		numbers
+
+	WHERE
+		number IN ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+
+	ORDER BY row, loc
+),
+number_group AS (
+	SELECT
+		row,
+		loc,
+		CASE WHEN loc =  lag + 1 THEN 0 ELSE 1 END AS group_bump,
+		number
+		
+	FROM 
+		number_filter
+
+	ORDER BY row, loc
+),
+number_groups AS (
+	SELECT
+		row,
+		CAST(loc AS INT) AS loc,
+		SUM(group_bump) OVER (ORDER BY row, loc) AS ngroup,
+		number
+		
+	FROM 
+		number_group
+
+	ORDER BY row, loc
+),
+number_candidates AS (
+	SELECT
+		row,
+		min(loc) OVER (PARTITION BY row, ngroup) AS loc_start,
+		max(loc) OVER (PARTITION BY row, ngroup) AS loc_end,
+		CAST(string_agg(number, '') OVER (PARTITION BY row, ngroup ORDER BY row, ngroup, loc) AS INT) AS number
+
+	FROM
+		number_groups
+
+	ORDER BY row, loc
+),
+number_location AS (
+	SELECT DISTINCT
+		row,
+		loc_start,
+		loc_end,
+		MAX(number) OVER (PARTITION BY row, loc_start, loc_end) AS number
+
+	FROM
+		number_candidates
+
+	ORDER BY row, loc_start
+),
+symbols AS (
+	SELECT
+		row,
+		unnest(schematics) AS symbol,
+		generate_subscripts(schematics, 1) AS loc
+	FROM
+		import
+),
+symbol_location AS (
+	SELECT DISTINCT
+		row AS row,
+		loc,
+		symbol
+	FROM 
+		symbols
+	WHERE 
+		symbols.symbol NOT IN ('.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+),
+list AS (
+	SELECT
+		symbol,
+		symbol_location.row,
+		symbol_location.loc,
+		number_location.number
+	FROM number_location
+	JOIN symbol_location 
+		ON symbol_location.row = number_location.row
+		AND
+		symbol_location.loc BETWEEN number_location.loc_start - 1 AND number_location.loc_end + 1
+		AND
+		symbol_location.symbol = '*'
+
+	UNION
+
+	SELECT 
+		symbol,
+		symbol_location.row,
+		symbol_location.loc,
+		number_location.number
+	FROM number_location
+	JOIN symbol_location 
+		ON symbol_location.row = number_location.row + 1
+		AND
+		symbol_location.loc BETWEEN number_location.loc_start - 1 AND number_location.loc_end + 1
+		AND
+		symbol_location.symbol = '*'
+
+	UNION
+
+	SELECT 
+		symbol,
+		symbol_location.row,
+		symbol_location.loc,
+		number_location.number
+	FROM number_location
+	JOIN symbol_location 
+		ON symbol_location.row = number_location.row - 1
+		AND
+		symbol_location.loc BETWEEN number_location.loc_start - 1 AND number_location.loc_end + 1
+		AND
+		symbol_location.symbol = '*'
+),
+possible_gears AS (
+	SELECT 
+		symbol,
+		row,
+		loc,
+		number,
+		ROW_NUMBER() OVER (PARTITION BY symbol, row, loc ORDER BY symbol, row, loc) AS num
+	FROM list
+	ORDER BY symbol, row, loc
+),
+gear_ratios AS (
+SELECT 
+	possible_gears.symbol,
+	possible_gears.row,
+	possible_gears.loc,
+	possible_gears.number AS gear1,
+	possible_gears2.number AS gear2,
+	IFNULL(possible_gears.number, 0) * IFNULL(possible_gears2.number, 0) AS ratio
+
+	FROM possible_gears
+	JOIN possible_gears AS possible_gears2 
+	ON 
+		possible_gears.row = possible_gears2.row
+		AND 
+		possible_gears.loc = possible_gears2.loc
+		AND
+		possible_gears2.num = 2
+		AND
+		possible_gears.number <> possible_gears2.number
+)
+
+SELECT 
+	467835 AS answer,
+	SUM(ratio) AS solution_part2,
+	467835 = SUM(ratio) AS right
+FROM gear_ratios
 ;
